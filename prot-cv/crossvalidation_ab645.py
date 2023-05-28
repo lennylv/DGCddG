b7cc356b48d18baa2d33dbcc8593dab926887603
Qiang Lyu <lennylv@hotmail.com>
2023-05-28 15:27:27 +0800

    Add files via upload

--- prot-cv/crossvalidation_ab645.py
+++ prot-cv/crossvalidation_ab645.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
 os.system('python cv_fold_645.py --fold 0 --validation 1')
-epoch = np.load('best_epoch.npy')
+epoch = np.load('best_epoch.npy')[0]
 for i in range(5):
     os.system('python cv_fold_645.py --fold ' + str(i) + ' --epoch '+str(epoch))
